b6b25e5e215b877a8af44ecfb5d86ca653cdb0da
Vansh Chaurasiya <vanshchaurasiya1557@gmail.com>
2024-09-16 03:34:49 +0530

    🚀Added codes

---
+++ Basics/var1.php
@@ -0,0 +1,7 @@
+<?php
+$str = "raining";
+echo "It is $str!";
+echo "\n";
+echo 'It is $str!'; // this shows a different result; single inverted code don't show value of variable
+echo "\n";
+echo "It is ".$str."!";
